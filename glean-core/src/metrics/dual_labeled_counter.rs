d7cbd4d39a55b4cd92fca29dfbed726197567aed
Jan-Erik Rediger <jrediger@mozilla.com>
2026-02-04 14:52:19 +0100

    Remove now-unused code

--- glean-core/src/metrics/dual_labeled_counter.rs
+++ glean-core/src/metrics/dual_labeled_counter.rs
@@ -12,9 +12,9 @@ use rusqlite::{params, Transaction};
 
 use crate::common_metric_data::{CommonMetricData, CommonMetricDataInternal, DynamicLabelType};
 use crate::error_recording::{
-    record_error, record_error_sqlite, test_get_num_recorded_errors, ErrorType,
+    record_error_sqlite, test_get_num_recorded_errors, ErrorType,
 };
-use crate::metrics::{CounterMetric, Metric, MetricType};
+use crate::metrics::{CounterMetric, MetricType};
 use crate::{Glean, TestGetValue};
 
 const MAX_LABELS: usize = 16;
@@ -254,38 +254,6 @@ impl TestGetValue for DualLabeledCounterMetric {
     }
 }
 
-/// Combines a metric's base identifier and label
-pub fn combine_base_identifier_and_labels(
-    base_identifier: &str,
-    key: &str,
-    category: &str,
-) -> String {
-    format!(
-        "{}{}",
-        base_identifier,
-        make_label_from_key_and_category(key, category)
-    )
-}
-
-/// Separate label into key and category components.
-/// Must validate the label format before calling this to ensure it doesn't contain
-/// any ASCII record separator characters aside from the one's we put there.
-pub fn separate_label_into_key_and_category(label: &str) -> Option<(&str, &str)> {
-    label
-        .strip_prefix(RECORD_SEPARATOR)
-        .unwrap_or(label)
-        .split_once(RECORD_SEPARATOR)
-}
-
-/// Construct and return a label from a given key and category with the RECORD_SEPARATOR
-/// characters in the format: `<RS><key><RS><category>`
-pub fn make_label_from_key_and_category(key: &str, category: &str) -> String {
-    format!(
-        "{}{}{}{}",
-        RECORD_SEPARATOR, key, RECORD_SEPARATOR, category
-    )
-}
-
 /// Validates a dynamic label, changing it to `OTHER_LABEL` if it's invalid.
 ///
 /// Checks the requested label against limitations, such as the label length and allowed
@@ -300,89 +268,12 @@ pub fn make_label_from_key_and_category(key: &str, category: &str) -> String {
 /// The entire identifier for the metric, including the base identifier and the corrected label.
 /// The errors are logged.
 pub fn validate_dynamic_key_and_or_category(
-    glean: &Glean,
-    meta: &CommonMetricDataInternal,
-    base_identifier: &str,
-    label: DynamicLabelType,
+    _glean: &Glean,
+    _meta: &CommonMetricDataInternal,
+    _base_identifier: &str,
+    _label: DynamicLabelType,
 ) -> String {
     panic!("not validating dual labeled like this anymore");
-    /*
-    // We should have exactly 3 elements when splitting by `RECORD_SEPARATOR`, since the label should begin with one and
-    // then the key and category are separated by one. Split should contain an empty string, the key, and the category.
-    // If we have more than 3 elements, then the consuming app must have used this character as part of a label and we
-    // cannot determine whether it was the key or the category at this point, so we record an `InvalidLabel` error and
-    // return `OTHER_LABEL` for both key and category.
-    if label.split(RECORD_SEPARATOR).count() != 3 {
-        let msg = "Label cannot contain the ASCII record separator character (0x1E)".to_string();
-        record_error(glean, meta, ErrorType::InvalidLabel, msg, None);
-        return combine_base_identifier_and_labels(base_identifier, OTHER_LABEL, OTHER_LABEL);
-    }
-
-    // Pick out the key and category from the supplied label
-    if let Some((mut key, mut category)) = separate_label_into_key_and_category(&label) {
-        // Loop through the stores we expect to find this metric in, and if we
-        // find it then just return the full metric identifier that was found
-        for store in &meta.inner.send_in_pings {
-            if glean.storage().has_metric(meta.inner.lifetime, store, key) {
-                return combine_base_identifier_and_labels(base_identifier, key, category);
-            }
-        }
-
-        // Count the number of distinct keys and categories already recorded, we can figure out which
-        // one(s) to check based on the label variant.
-        let (seen_keys, seen_categories) = get_seen_keys_and_categories(meta, glean);
-        match label {
-            DynamicLabelType::Static(_) => todo!(),
-            DynamicLabelType::Label(ref label) => {
-                record_error(
-                    glean,
-                    meta,
-                    ErrorType::InvalidLabel,
-                    format!("Invalid `DualLabeledCounter` label format: {label:?}"),
-                    None,
-                );
-                key = OTHER_LABEL;
-                category = OTHER_LABEL;
-            }
-            DynamicLabelType::KeyOnly(_) => {
-                if (!seen_keys.contains(key) && seen_keys.len() >= MAX_LABELS)
-                    || !label_is_valid(key, glean, meta)
-                {
-                    key = OTHER_LABEL;
-                }
-            }
-            DynamicLabelType::CategoryOnly(_) => {
-                if (!seen_categories.contains(category) && seen_categories.len() >= MAX_LABELS)
-                    || !label_is_valid(category, glean, meta)
-                {
-                    category = OTHER_LABEL;
-                }
-            }
-            DynamicLabelType::KeyAndCategory(_) => {
-                if (!seen_keys.contains(key) && seen_keys.len() >= MAX_LABELS)
-                    || !label_is_valid(key, glean, meta)
-                {
-                    key = OTHER_LABEL;
-                }
-                if (!seen_categories.contains(category) && seen_categories.len() >= MAX_LABELS)
-                    || !label_is_valid(category, glean, meta)
-                {
-                    category = OTHER_LABEL;
-                }
-            }
-        }
-        combine_base_identifier_and_labels(base_identifier, key, category)
-    } else {
-        record_error(
-            glean,
-            meta,
-            ErrorType::InvalidLabel,
-            "Invalid `DualLabeledCounter` label format, unable to determine key and/or category",
-            None,
-        );
-        combine_base_identifier_and_labels(base_identifier, OTHER_LABEL, OTHER_LABEL)
-    }
-    */
 }
 
 pub fn validate_dual_label_sqlite(
@@ -436,8 +327,8 @@ pub fn validate_dual_label_sqlite(
         }
     }
 
-    let mut new_key;
-    let mut new_category;
+    let new_key;
+    let new_category;
 
     if existing_keys.contains(key) || existing_keys.len() < MAX_LABELS {
         new_key = label_is_valid_sqlite(key, tx, base_identifier, send_in_pings);
@@ -489,56 +380,4 @@ fn label_is_valid_sqlite<'a>(
     } else {
         label
     }
-}
-
-fn label_is_valid(label: &str, glean: &Glean, meta: &CommonMetricDataInternal) -> bool {
-    if label.len() > MAX_LABEL_LENGTH {
-        let msg = format!(
-            "label length {} exceeds maximum of {}",
-            label.len(),
-            MAX_LABEL_LENGTH
-        );
-        record_error(glean, meta, ErrorType::InvalidLabel, msg, None);
-        false
-    } else {
-        true
-    }
-}
-
-fn get_seen_keys_and_categories(
-    meta: &CommonMetricDataInternal,
-    glean: &Glean,
-) -> (HashSet<String>, HashSet<String>) {
-    let base_identifier = &meta.base_identifier();
-    let prefix = format!("{base_identifier}{RECORD_SEPARATOR}");
-    let mut seen_keys: HashSet<String> = HashSet::new();
-    let mut seen_categories: HashSet<String> = HashSet::new();
-    let mut snapshotter = |metric_id: &[u8], _: &Metric| {
-        let metric_id_str = String::from_utf8_lossy(metric_id);
-
-        // Split full identifier on the ASCII Record Separator (\x1e)
-        let parts: Vec<&str> = metric_id_str.split(RECORD_SEPARATOR).collect();
-
-        if parts.len() == 2 {
-            seen_keys.insert(parts[0].into());
-            seen_categories.insert(parts[1].into());
-        } else {
-            record_error(
-                glean,
-                meta,
-                ErrorType::InvalidLabel,
-                "Dual Labeled Counter label doesn't contain exactly 2 parts".to_string(),
-                None,
-            );
-        }
-    };
-
-    let lifetime = meta.inner.lifetime;
-    for store in &meta.inner.send_in_pings {
-        glean
-            .storage()
-            .iter_store_from(lifetime, store, &prefix, &mut snapshotter);
-    }
-
-    (seen_keys, seen_categories)
 }
