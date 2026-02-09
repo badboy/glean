e51df7fdc93ea77f08061eded4068dd742025c37
Jan-Erik Rediger <jrediger@mozilla.com>
2026-02-09 21:12:53 +0100

    limit this to the gungraun feature

--- glean-core/benchmark/benches/bin-bench.rs
+++ glean-core/benchmark/benches/bin-bench.rs
@@ -1,7 +1,15 @@
+#[cfg(feature = "gungraun")]
 use std::env;
 
+#[cfg(feature = "gungraun")]
 use gungraun::{binary_benchmark, binary_benchmark_group, main};
 
+#[cfg(not(feature = "gungraun"))]
+fn main() {
+    eprintln!("--feature gungraun required. Linux only.");
+}
+
+#[cfg(feature = "gungraun")]
 #[binary_benchmark]
 fn bench_binary() -> gungraun::Command {
     gungraun::Command::new(env::var("RAPID_METRICS_EXE").expect("need path in RAPID_METRICS_EXE"))
@@ -9,9 +17,11 @@ fn bench_binary() -> gungraun::Command {
         .build()
 }
 
+#[cfg(feature = "gungraun")]
 binary_benchmark_group!(
     name = glean;
     benchmarks = bench_binary
 );
 
+#[cfg(feature = "gungraun")]
 main!(binary_benchmark_groups = glean);
